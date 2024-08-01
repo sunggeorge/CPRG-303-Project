41f38c3082751eb4194b47409f811021e874a38b
willhui1204 <siukee.hui@edu.sait.ca>
2024-08-01 16:13:50 -0600

    month selector added

--- src/scenes/history/History.js
+++ src/scenes/history/History.js
@@ -5,7 +5,7 @@ import IconButton from '../../components/IconButton'
 import ScreenTemplate from '../../components/ScreenTemplate'
 import Button from '../../components/Button'
 import { firestore } from '../../firebase/config'
-import { doc, onSnapshot } from 'firebase/firestore';
+import { doc, onSnapshot, Timestamp } from 'firebase/firestore';
 import { colors, fontSize } from '../../theme'
 import { UserDataContext } from '../../context/UserDataContext'
 import { ColorSchemeContext } from '../../context/ColorSchemeContext'
@@ -13,6 +13,8 @@ import { sendNotification } from '../../utils/SendNotification'
 import { getKilobyteSize } from '../../utils/functions'
 import { useAtom } from 'jotai'
 import { transactionsAtom } from '../../utils/atom'
+import { format, addMonths, isSameMonth } from 'date-fns'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 
 export default function History() {
@@ -27,6 +29,9 @@ export default function History() {
     text: isDark? colors.white : colors.primaryText
   }
 
+  // ***Month selector
+  const [selectedDate, setSelectedDate] = useState(new Date())
+  const [currentMonthTransactions, setCurrentMonthTransactions] = useState([])
 
 
   // useLayoutEffect(() => {
@@ -70,12 +75,49 @@ export default function History() {
     console.log(res)
   }
 
+  // ***Month selector
+  useEffect(() => {
+    loadCurrentMonthTransactions(selectedDate)
+  }, [selectedDate, transactions])
+
+  const handlePreviousMonth = () => {
+    setSelectedDate(addMonths(selectedDate, -1))
+  }
+
+  const handleNextMonth = () => {
+    setSelectedDate(addMonths(selectedDate, 1))
+  }
+
+  const convertFirestoreTimestampToDate = (timestamp) => {
+    return new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000)
+  } 
+
+  const loadCurrentMonthTransactions = (date) => {
+    const filteredTransactions = transactions.filter(transaction =>
+      isSameMonth(convertFirestoreTimestampToDate(transaction.date), date)
+     )
+     setCurrentMonthTransactions(filteredTransactions)
+  }
+
 // Assuming transactions is an array of transaction objects
 const sortedTransactions = transactions.sort((a, b) => b.date.seconds - a.date.seconds);
 
+// *****
+const isCurrentMonth = isSameMonth(selectedDate, new Date())
 
   return (
     <ScreenTemplate>
+      {/* *** */}
+      <View style={styles.monthSelector}>
+      <TouchableOpacity onPress={handlePreviousMonth}>
+        <Text style={styles.arrow}>&#9664;</Text>
+      </TouchableOpacity>
+      <Text style={styles.month}>{format(selectedDate, 'MMMM, yyyy')}</Text>
+      <TouchableOpacity onPress={handleNextMonth} disabled={isCurrentMonth}>
+        <Text style={[styles.arrow, isCurrentMonth && styles.disabled]}>&#9654;</Text>
+      </TouchableOpacity>
+    </View>
+      
       <ScrollView style={styles.main}>
       {sortedTransactions.map((transaction) => (
         <View key={transaction.id} style={styles.transactionItem}>
@@ -127,5 +169,27 @@ const styles = StyleSheet.create({
     borderColor: colors.lightPurple,
     borderRadius: 5,
     marginBottom: 10,
+  },
+  // ***
+  monthSelector: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 10,
+    marginBottom: 10
+  },
+  arrow: {
+    fontSize: 20,
+    color: colors.primary
+  },
+  month: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  disabled: {
+    color: '#ccc'
   }
+
 })
